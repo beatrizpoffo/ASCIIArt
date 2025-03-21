9d5b897b3cae6cd28b578bcc3db096582efaca28
beatrizpoffo <beatrizpoffo7a5@gmail.com>
2025-03-21 02:39:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("               ((`\\");
+            Console.WriteLine("            ___ \\ '--._");
+            Console.WriteLine("         .'`   `'    o  )");
+            Console.WriteLine("        /    \\   '. __.'");
+            Console.WriteLine("       _|    /_  \\ \\_\\_");
+            Console.WriteLine("jgs   {_\\______\\-'\\__\\_\\");
+        }
+    }
+}
